1b3552c666f0ed762df1c17fa693bcf26c53fdf1
Francis Reyes <francis.mark.reyes@gmail.com>
2015-02-08 00:11:56 +1100

    Commit while it works

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM dockerfile/ansible:latest
 MAINTAINER Francis Reyes <freyes@snowballone.com.au>
 ADD ansible /ansible
 COPY run.sh /run.sh
-CMD ["/run.sh"]
+RUN ["/run.sh", "-e", "build_image=true"]
+ENTRYPOINT ["/run.sh"]
+CMD ["/usr/sbin/mysqld"]
 VOLUME ["/etc/mysql", "/var/lib/mysql", "/root"]
+WORKDIR /data
 EXPOSE 3306
--- run.sh
+++ run.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 set -e
 
+COMMAND=/usr/local/bin/ansible-playbook
+echo "{}" > /ansible/vars.json
+
 ANSIBLE_ARGS="-v /ansible/playbook.yml --verbose -i 127.0.0.1,"
+
+# $ANSIBLE_EXTRA_VARS is set by fig
 if [ "$ANSIBLE_EXTRA_VARS" ]; then
+    # clean-up the JSON stored in the variable and save to vars.json
     echo "$ANSIBLE_EXTRA_VARS" | sed s#\'#\"#g | python -mjson.tool > /ansible/vars.json
-    ANSIBLE_ARGS="${ANSIBLE_ARGS} --extra-vars '@vars.json'"
+    ANSIBLE_ARGS="${ANSIBLE_ARGS} -e '@vars.json'"
 fi
 
-ansible-playbook --version
-ansible-playbook $ANSIBLE_ARGS
+echo "$ANSIBLE_ARGS"
 
-/usr/sbin/mysqld
+# if first argument begins with a -
+if [ "${1:0:1}" = '-' ]; then
+    # set ansible-playbook as the executable
+    set -- $COMMAND "$@" $ANSIBLE_ARGS
+else
+    # ensure ansible-playbook runs to provision the container *before* running
+    # mysqld
+    echo "$COMMAND $ANSIBLE_ARGS"
+    $COMMAND $ANSIBLE_ARGS
+fi
+
+exec $@
